be082990f3af7e57cd77506c15e81dd0fb024c3f
Inuka ASiTH <inukaasith7@gmail.com>
2021-03-29 18:50:48 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -317,31 +317,6 @@ async def _(client,message):
                 await message.reply(pro)
 
 
-__help__ = """
-<b> Chatbot </b>
-<i> PRESENTING DAISY AI 3.0. THE ONLY AI SYSTEM WHICH CAN DETECT & REPLY UPTO 200 LANGUAGES </i>
- - /chatbot <i>ON/OFF</i>: Enables and disables AI Chat mode (EXCLUSIVE)
-* DaisyAI can detect and reply upto 200 languages by now *
- - /chatbot EN : Enables English only chatbot
- 
-<b> Lydia </b>
-<i> PRESENTING DAISY'S LYDIA, EXCLUSIVE CHAT FEATURE DETECT UPTO 200 LANGUAGES & REPLY USING LYDIA AI</i>
- - /addlydia: Activates lydia on your group
-* Daisy AI can detect and reply upto 200 languages by now *
- - /enlydia : Enables English only chat AI
- - /rmlydia : Deactivates lydia on your group (UNSTABLE)
- 
-<b> Assistant </b>
- - /ask <i>question</i>: Ask question from daisy
- - /ask <i> reply to voice note</i>: Get voice reply
- 
-<i> Lydia AI can be unstable sometimes </i>
-"""
-
-__mod_name__ = "AI Assistant"           
-
-
-
 
 
 #    Copyright (C) 2020-2021 by @InukaAsith
