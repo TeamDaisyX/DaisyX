f32510359995cb309e9bdbada7d98b8785d639a9
InukaAsith <inukaasith7@gmail.com>
2021-04-02 08:42:05 +0000

    Auto Fixes: Code formatted

--- DaisyX/modules/tagall.py
+++ DaisyX/modules/tagall.py
@@ -1,5 +1,3 @@
-
-
 # Copyright (C) 2020-2021 by DevsExpo@Github, < https://github.com/DevsExpo >.
 #
 # This file is part of < https://github.com/DevsExpo/FridayUserBot > project,
@@ -9,23 +7,24 @@
 # All rights reserved.
 
 
-
-from DaisyX.services.pyrogram import pbot
 from pyrogram import filters
+
 from DaisyX.function.pluginhelpers import admins_only
+from DaisyX.services.pyrogram import pbot
+
 
 @pbot.on_message(filters.command("tagall") & ~filters.edited & ~filters.bot)
 @admins_only
 async def tagall(client, message):
-    pablo = await message.reply("`Processing.....`")
+    await message.reply("`Processing.....`")
     sh = message.text
     if not sh:
         sh = "Hi!"
-    mentions =""
+    mentions = ""
     async for member in client.iter_chat_members(message.chat.id):
         mentions += member.user.mention + " "
     n = 4096
-    kk = [mentions[i:i+n] for i in range(0, len(mentions), n)]
+    kk = [mentions[i : i + n] for i in range(0, len(mentions), n)]
     for i in kk:
         j = f"<b>{sh}</b> \n{i}"
         await client.send_message(message.chat.id, j, parse_mode="html")
