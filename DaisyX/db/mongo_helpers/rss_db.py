02a3ac80dae6d06f95908fe5c0a26c0b9c1bb2e4
InukaAsith <inukaasith7@gmail.com>
2021-04-02 10:00:54 +0000

    Auto Fixes: Code formatted

--- DaisyX/db/mongo_helpers/rss_db.py
+++ DaisyX/db/mongo_helpers/rss_db.py
@@ -4,19 +4,24 @@ rss = db_x["RSS"]
 
 
 async def add_rss(chat_id, rss_link, latest_rss):
-    await rss.insert_one({"chat_id": chat_id, "rss_link": rss_link, "latest_rss": latest_rss})
-         
+    await rss.insert_one(
+        {"chat_id": chat_id, "rss_link": rss_link, "latest_rss": latest_rss}
+    )
+
 
 async def del_rss(chat_id, rss_link):
     await rss.delete_one({"chat_id": chat_id, "rss_link": rss_link})
+
 
 async def get_chat_rss(chat_id):
     lol = [m async for m in rss.find({"chat_id": chat_id})]
     return lol
 
-async def update_rss(chat_id, rss_link, latest_rss):
-    await rss.update_one({"chat_id": chat_id, "rss_link": rss_link}, {"$set": {"latest_rss": latest_rss}})
 
+async def update_rss(chat_id, rss_link, latest_rss):
+    await rss.update_one(
+        {"chat_id": chat_id, "rss_link": rss_link}, {"$set": {"latest_rss": latest_rss}}
+    )
 
 
 async def is_get_chat_rss(chat_id, rss_link):
@@ -25,6 +30,7 @@ async def is_get_chat_rss(chat_id, rss_link):
         return True
     else:
         return False
+
 
 async def basic_check(chat_id):
     lol = await rss.find_one({"chat_id": chat_id})
@@ -37,7 +43,7 @@ async def basic_check(chat_id):
 async def overall_check():
     lol = await rss.find_one()
     if lol:
-        return True 
+        return True
     else:
         return False
 
@@ -46,6 +52,6 @@ async def get_all():
     lol = [rrrs async for rrrs in rss.find()]
     return lol
 
+
 async def delete_all():
-    lol = await rss.delete_many({})
-    
+    await rss.delete_many({})
