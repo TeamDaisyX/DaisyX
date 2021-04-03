8fc8d16c0657c6c35ab7e0df22aed11dad9ce88a
Inuka ASiTH <inukaasith7@gmail.com>
2021-04-03 16:52:41 +0530

    Update aichat.py

--- DaisyX/db/mongo_helpers/aichat.py
+++ DaisyX/db/mongo_helpers/aichat.py
@@ -3,34 +3,34 @@ from DaisyX.services.mongo import mongodb as db_x
 lydia = db_x["CAHTBOT"]
 
 
-async def add_chat(chat_id, session_id):
-    stark = await lydia.find_one({"chat_id": chat_id})
+def add_chat(chat_id, session_id):
+    stark = lydia.find_one({"chat_id": chat_id})
     if stark:
         return False
     else:
-        await lydia.insert_one({"chat_id": chat_id})
+        lydia.insert_one({"chat_id": chat_id})
         return True
 
 
-async def remove_chat(chat_id):
-    stark = await lydia.find_one({"chat_id": chat_id})
+def remove_chat(chat_id):
+    stark = lydia.find_one({"chat_id": chat_id})
     if not stark:
         return False
     else:
-        await lydia.delete_one({"chat_id": chat_id})
+        lydia.delete_one({"chat_id": chat_id})
         return True
 
 
-async def get_all_chats():
-    r = [o async for o in lydia.find()]
+def get_all_chats():
+    r = list(lydia.find())
     if r:
         return r
     else:
         return False
 
 
-async def get_session(chat_id):
-    stark = await lydia.find_one({"chat_id": chat_id})
+def get_session(chat_id):
+    stark = lydia.find_one({"chat_id": chat_id})
     if not stark:
         return False
     else:
