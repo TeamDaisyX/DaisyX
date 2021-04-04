ca390821428ccdc78e2cc3112e84ed9e2a533c65
Inuka ASiTH <inukaasith7@gmail.com>
2021-04-04 14:32:10 +0530

    Update rss_db.py

--- DaisyX/db/mongo_helpers/rss_db.py
+++ DaisyX/db/mongo_helpers/rss_db.py
@@ -3,55 +3,50 @@ from DaisyX.services.mongo import mongodb as db_x
 rss = db_x["RSS"]
 
 
-async def add_rss(chat_id, rss_link, latest_rss):
-    await rss.insert_one(
-        {"chat_id": chat_id, "rss_link": rss_link, "latest_rss": latest_rss}
-    )
+def add_rss(chat_id, rss_link, latest_rss):
+    rss.insert_one({"chat_id": chat_id, "rss_link": rss_link, "latest_rss": latest_rss})
+         
 
+def del_rss(chat_id, rss_link):
+    rss.delete_one({"chat_id": chat_id, "rss_link": rss_link})
 
-async def del_rss(chat_id, rss_link):
-    await rss.delete_one({"chat_id": chat_id, "rss_link": rss_link})
-
-
-async def get_chat_rss(chat_id):
-    lol = [m async for m in rss.find({"chat_id": chat_id})]
+def get_chat_rss(chat_id):
+    lol = list(rss.find({"chat_id": chat_id}))
     return lol
 
-
-async def update_rss(chat_id, rss_link, latest_rss):
-    await rss.update_one(
-        {"chat_id": chat_id, "rss_link": rss_link}, {"$set": {"latest_rss": latest_rss}}
-    )
+def update_rss(chat_id, rss_link, latest_rss):
+    rss.update_one({"chat_id": chat_id, "rss_link": rss_link}, {"$set": {"latest_rss": latest_rss}})
 
 
-async def is_get_chat_rss(chat_id, rss_link):
-    lol = await rss.find_one({"chat_id": chat_id, "rss_link": rss_link})
+
+def is_get_chat_rss(chat_id, rss_link):
+    lol = rss.find_one({"chat_id": chat_id, "rss_link": rss_link})
+    if lol:
+        return True
+    else:
+        return False
+
+def basic_check(chat_id):
+    lol = rss.find_one({"chat_id": chat_id})
     if lol:
         return True
     else:
         return False
 
 
-async def basic_check(chat_id):
-    lol = await rss.find_one({"chat_id": chat_id})
+def overall_check():
+    lol = rss.find_one()
     if lol:
-        return True
+        return True 
     else:
         return False
 
 
-async def overall_check():
-    lol = await rss.find_one()
-    if lol:
-        return True
-    else:
-        return False
-
-
-async def get_all():
-    lol = [rrrs async for rrrs in rss.find()]
+def get_all():
+    lol = rss.find()
     return lol
 
+def delete_all():
+    lol = rss.delete_many({})
+    
 
-async def delete_all():
-    await rss.delete_many({})
