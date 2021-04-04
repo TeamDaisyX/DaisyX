eac7adc7fe5e4606831cdc712a95dbcf258c03b2
Inuka ASiTH <inukaasith7@gmail.com>
2021-04-04 15:40:00 +0530

    Update quotely.py

--- DaisyX/modules/quotely.py
+++ DaisyX/modules/quotely.py
@@ -1,6 +1,6 @@
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
-
+from DaisyX.services.telethonuserbot import ubot
 from DaisyX.services.events import register
 
 
