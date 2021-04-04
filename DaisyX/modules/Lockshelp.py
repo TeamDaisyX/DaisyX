e1d77d4ecbfe304b70b17df50269f4f2454135b9
Inuka ASiTH <inukaasith7@gmail.com>
2021-04-04 13:19:59 +0530

    Update Lockshelp.py

--- DaisyX/modules/Lockshelp.py
+++ DaisyX/modules/Lockshelp.py
@@ -1,1 +1,9 @@
-__mode_name__ = "Locks"
+__mod_name__ = "Locks"
+
+__help__ = """
+Use this feature to block users from sending specific message types to your group!
+<b>Available commands are:</b>
+- /locks or /locktypes: Use this command to know current state of your locks in your group!
+- /lock (locktype): Locks a type of messages
+- /unlock (locktype): Unlocks a type of message
+"""
