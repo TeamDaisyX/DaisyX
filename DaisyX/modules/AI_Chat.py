43ff6bf81a4510b37383ac55ccb8956b666906d8
Inuka ASiTH <inukaasith7@gmail.com>
2021-04-05 12:04:57 +0530

    Update AI_Chat.py

--- DaisyX/modules/AI_Chat.py
+++ DaisyX/modules/AI_Chat.py
@@ -26,7 +26,7 @@ from google_trans_new import google_translator
 from pyrogram import filters
 
 from DaisyX import BOT_ID
-from DaisyX.db.mongo_helpers.aichat import add_chat, get_session, remove_chat
+from DaisyX.db.mongo_helpers.aichat import add_chat, get_session, remove_chat,add_chat_t,get_session_t,remove_chat_t
 from DaisyX.function.pluginhelpers import admins_only, edit_or_reply
 from DaisyX.services.pyrogram import pbot as daisyx
 
@@ -84,7 +84,48 @@ async def hmm(_, message):
         await message.reply_text(
             "I only recognize `/chatbot on` and /chatbot `off only`"
         )
+@daisyx.on_message(filters.command("talkmode") & ~filters.edited & ~filters.bot)
+@admins_only
+async def hmm(_, message):
+    global daisy_chats
+    if len(message.command) != 2:
+        await message.reply_text(
+            "I only recognize `/talkmode on` and /talkmode `off only`"
+        )
+        message.continue_propagation()
+    status = message.text.split(None, 1)[1]
+    chat_id = message.chat.id
+    if status == "ON" or status == "on" or status == "On":
+        lel = await edit_or_reply(message, "`Processing...`")
+        lol = add_chat_t(int(message.chat.id))
+        if not lol:
+            await lel.edit("Daisy AI Already Activated In This Chat")
+            return
+        await lel.edit(
+            f"Daisy AI Successfully Added For Users In The Chat {message.chat.id}"
+        )
 
+    elif status == "OFF" or status == "off" or status == "Off":
+        lel = await edit_or_reply(message, "`Processing...`")
+        Escobar = remove_chat_t(int(message.chat.id))
+        if not Escobar:
+            await lel.edit("Daisy AI Was Not Activated In This Chat")
+            return
+        await lel.edit(
+            f"Daisy AI Successfully Deactivated For Users In The Chat {message.chat.id}"
+        )
+
+    elif status == "EN" or status == "en" or status == "english":
+        if not chat_id in en_chats:
+            en_chats.append(chat_id)
+            await message.reply_text("English AI chat Enabled!")
+            return
+        await message.reply_text("AI Chat Is Already Disabled.")
+        message.continue_propagation()
+    else:
+        await message.reply_text(
+            "I only recognize `/chatbot on` and /chatbot `off only`"
+        )
 
 @daisyx.on_message(
     filters.text & filters.reply & ~filters.bot & ~filters.via_bot & ~filters.forwarded,
@@ -185,12 +226,192 @@ async def hmm(client, message):
         pro = result
         if not "en" in lan and not lan == "":
             pro = translator.translate(pro, lang_tgt=lan[0])
+        if get_session_t(int(message.chat.id)):
+            answer = pro
+            try:
+                tts = gTTS(answer, tld="com", lang=lan[0])
+                tts.save("results.mp3")
+            except AssertionError:
+                message.continue_propagation()
+            except ValueError:
+                message.continue_propagation()
+            except RuntimeError:
+                message.continue_propagation()
+            except gTTSError:
+                message.continue_propagation()
+            with open("results.mp3", "r"):
+                await client.send_voice(
+                    message.chat.id,
+                    "results.mp3",
+                    reply_to_message_id=message.message_id,
+                )
+            os.remove("results.mp3")
+            # Sending final
+        else:            
+            try:
+                await daisyx.send_chat_action(message.chat.id, "typing")
+                await message.reply_text(pro)
+            except CFError as e:
+                print(e)
+
+                
+ #TALK BITCH
+
+@daisyx.on_message(
+    filters.voice & filters.reply & ~filters.bot & ~filters.via_bot & ~filters.forwarded,
+    group=2,
+)
+async def hmm(client, message):
+    if not get_session(int(message.chat.id)):
+        message.continue_propagation()
+    if message.reply_to_message.from_user.id != BOT_ID:
+        message.continue_propagation()
+    previous_message = message
+    required_file_name = message.download()
+    if IBM_WATSON_CRED_URL is None or IBM_WATSON_CRED_PASSWORD is None:
+        await message.reply(
+            "You need to set the required ENV variables for this module. \nModule stopping"
+        )
+    else:
+        headers = {
+            "Content-Type": previous_message.voice.mime_type,
+        }
+        data = open(required_file_name, "rb").read()
+        response = requests.post(
+            IBM_WATSON_CRED_URL + "/v1/recognize",
+            headers=headers,
+            data=data,
+            auth=("apikey", IBM_WATSON_CRED_PASSWORD),
+        )
+        r = response.json()
+        print(r)
+    #appid = WOLFRAM_ID
+    #server = f"https://api.wolframalpha.com/v1/spoken?appid={appid}&i={i}"
+    #res = get(server)        appid = WOLFRAM_ID
+    #server = f"https://api.wolframalpha.com/v1/spoken?appid={appid}&i={i}"
+    #res = get(server)
+"""
+    msg = message.text
+    chat_id = message.chat.id
+    if msg.startswith("/") or msg.startswith("@"):
+        message.continue_propagation()
+    if chat_id in en_chats:
+        test = msg
+        test = test.replace("daisy", "Aco")
+        test = test.replace("Daisy", "Aco")
+        querystring = {
+            "bid": "178",
+            "key": "sX5A2PcYZbsN5EY6",
+            "uid": "mashape",
+            "msg": {test},
+        }
+        headers = {
+            "x-rapidapi-key": "cf9e67ea99mshecc7e1ddb8e93d1p1b9e04jsn3f1bb9103c3f",
+            "x-rapidapi-host": "acobot-brainshop-ai-v1.p.rapidapi.com",
+        }
+        response = requests.request("GET", url, headers=headers, params=querystring)
+        result = response.text
+        result = result.replace('{"cnt":"', "")
+        result = result.replace('"}', "")
+        result = result.replace("Aco", "Daisy")
+        result = result.replace("<a href=\\", "<a href =")
+        result = result.replace("<\/a>", "</a>")
+        pro = result
         try:
             await daisyx.send_chat_action(message.chat.id, "typing")
             await message.reply_text(pro)
         except CFError as e:
             print(e)
+    else:
+        u = msg.split()
+        emj = extract_emojis(msg)
+        msg = msg.replace(emj, "")
+        if (
+            [(k) for k in u if k.startswith("@")]
+            and [(k) for k in u if k.startswith("#")]
+            and [(k) for k in u if k.startswith("/")]
+            and re.findall(r"\[([^]]+)]\(\s*([^)]+)\s*\)", msg) != []
+        ):
 
+            h = " ".join(filter(lambda x: x[0] != "@", u))
+            km = re.sub(r"\[([^]]+)]\(\s*([^)]+)\s*\)", r"", h)
+            tm = km.split()
+            jm = " ".join(filter(lambda x: x[0] != "#", tm))
+            hm = jm.split()
+            rm = " ".join(filter(lambda x: x[0] != "/", hm))
+        elif [(k) for k in u if k.startswith("@")]:
+
+            rm = " ".join(filter(lambda x: x[0] != "@", u))
+        elif [(k) for k in u if k.startswith("#")]:
+            rm = " ".join(filter(lambda x: x[0] != "#", u))
+        elif [(k) for k in u if k.startswith("/")]:
+            rm = " ".join(filter(lambda x: x[0] != "/", u))
+        elif re.findall(r"\[([^]]+)]\(\s*([^)]+)\s*\)", msg) != []:
+            rm = re.sub(r"\[([^]]+)]\(\s*([^)]+)\s*\)", r"", msg)
+        else:
+            rm = msg
+            # print (rm)
+            lan = translator.detect(rm)
+        test = rm
+        if not "en" in lan and not lan == "":
+            test = translator.translate(test, lang_tgt="en")
+
+        # test = emoji.demojize(test.strip())
+
+        # Kang with the credits bitches @InukaASiTH
+        test = test.replace("daisy", "Aco")
+        test = test.replace("Daisy", "Aco")
+        querystring = {
+            "bid": "178",
+            "key": "sX5A2PcYZbsN5EY6",
+            "uid": "mashape",
+            "msg": {test},
+        }
+        headers = {
+            "x-rapidapi-key": "cf9e67ea99mshecc7e1ddb8e93d1p1b9e04jsn3f1bb9103c3f",
+            "x-rapidapi-host": "acobot-brainshop-ai-v1.p.rapidapi.com",
+        }
+        response = requests.request("GET", url, headers=headers, params=querystring)
+        result = response.text
+        result = result.replace('{"cnt":"', "")
+        result = result.replace('"}', "")
+        result = result.replace("Aco", "Daisy")
+        result = result.replace("<a href=\\", "<a href =")
+        result = result.replace("<\/a>", "</a>")
+        pro = result
+        if not "en" in lan and not lan == "":
+            pro = translator.translate(pro, lang_tgt=lan[0])
+        if get_session_t(int(message.chat.id)):
+            answer = pro
+            try:
+                tts = gTTS(answer, tld="com", lang=lan[0])
+                tts.save("results.mp3")
+            except AssertionError:
+                message.continue_propagation()
+            except ValueError:
+                message.continue_propagation()
+            except RuntimeError:
+                message.continue_propagation()
+            except gTTSError:
+                message.continue_propagation()
+            with open("results.mp3", "r"):
+                await client.send_voice(
+                    message.chat.id,
+                    "results.mp3",
+                    reply_to_message_id=message.message_id,
+                )
+            os.remove("results.mp3")
+            # Sending final
+        else:            
+            try:
+                await daisyx.send_chat_action(message.chat.id, "typing")
+                await message.reply_text(pro)
+            except CFError as e:
+                print(e)
+"""         
+                
+                
+                
 
 @daisyx.on_message(filters.text & filters.private & filters.reply & ~filters.bot)
 async def inuka(client, message):
