1745a943d099c820551278b02c783cf6ec9f0c62
Inuka ASiTH <inukaasith7@gmail.com>
2021-04-05 08:50:01 +0530

    Delete Lockshelp.py

--- DaisyX/modules/Lockshelp.py
+++
@@ -1,9 +0,0 @@
-__mod_name__ = "Locks"
-
-__help__ = """
-Use this feature to block users from sending specific message types to your group!
-<b>Available commands are:</b>
-- /locks or /locktypes: Use this command to know current state of your locks in your group!
-- /lock (locktype): Locks a type of messages
-- /unlock (locktype): Unlocks a type of message
-"""
