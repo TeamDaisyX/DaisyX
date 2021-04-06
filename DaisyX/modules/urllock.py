2db8049ade2b9afdebdc46e170cfbceada18fa12
Inuka ASiTH <inukaasith7@gmail.com>
2021-04-06 10:12:20 +0530

    Update urllock.py

--- DaisyX/modules/urllock.py
+++ DaisyX/modules/urllock.py
@@ -1,3 +1,18 @@
+#    Copyright (C) 2020-2021 by @InukaAsith
+
+#    This programme is a part of DaisyX (TG bot) project
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import asyncio
 
 from pyrogram import filters
