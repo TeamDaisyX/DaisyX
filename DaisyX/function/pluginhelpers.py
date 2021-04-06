e4030ce9639e4795a3d672cd94357399ddfb871e
Inuka ASiTH <inukaasith7@gmail.com>
2021-04-06 10:10:30 +0530

    Update pluginhelpers.py

--- DaisyX/function/pluginhelpers.py
+++ DaisyX/function/pluginhelpers.py
@@ -351,3 +351,31 @@ async def member_permissions(chat_id, user_id):
     if member.can_pin_messages:
         perms.append("can_pin_messages")
     return perms
+
+# URL LOCK
+
+def get_url(message_1: Message) -> Union[str, None]:
+    messages = [message_1]
+
+    if message_1.reply_to_message:
+        messages.append(message_1.reply_to_message)
+
+    text = ""
+    offset = None
+    length = None
+
+    for message in messages:
+        if offset:
+            break
+
+        if message.entities:
+            for entity in message.entities:
+                if entity.type == "url":
+                    text = message.text or message.caption
+                    offset, length = entity.offset, entity.length
+                    break
+
+    if offset in (None,):
+        return None
+
+    return text[offset:offset + length]
