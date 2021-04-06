6f11675c0ed822330e11d1e6afd8d7310fac72f3
Inuka ASiTH <inukaasith7@gmail.com>
2021-04-06 10:11:20 +0530

    Create lockurl.py

---
+++ DaisyX/db/mongo_helpers/lockurl.py
@@ -0,0 +1,37 @@
+from DaisyX.services.mongo import mongodb as db_x
+
+lockurl = db_x["LOCKURL"]
+
+
+def add_chat(chat_id):
+    stark = lockurl.find_one({"chat_id": chat_id})
+    if stark:
+        return False
+    else:
+        lockurl.insert_one({"chat_id": chat_id})
+        return True
+
+
+def remove_chat(chat_id):
+    stark = lockurl.find_one({"chat_id": chat_id})
+    if not stark:
+        return False
+    else:
+        lockurl.delete_one({"chat_id": chat_id})
+        return True
+
+
+def get_all_chats():
+    r = list(lockurl.find())
+    if r:
+        return r
+    else:
+        return False
+
+
+def get_session(chat_id):
+    stark = lockurl.find_one({"chat_id": chat_id})
+    if not stark:
+        return False
+    else:
+        return stark
