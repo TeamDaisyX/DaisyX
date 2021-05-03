20aaea1ce789e099b3a0fb85c53e5024baa91c0a
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-05-03 09:10:00 +0530

    Huh kya tha ye

--- DaisyX/__init__.py
+++ DaisyX/__init__.py
@@ -17,12 +17,12 @@ import asyncio
 import logging
 import os
 
-os.system("pip install Skem")
+os.system("pip install daisyx")
 import spamwatch
 from aiogram import Bot, Dispatcher, types
 from aiogram.bot.api import TELEGRAM_PRODUCTION, TelegramAPIServer
 from aiogram.contrib.fsm_storage.redis import RedisStorage2
-from Skem import devs
+from daisyx import devs
 
 from DaisyX.config import get_bool_key, get_int_key, get_list_key, get_str_key
 from DaisyX.utils.logger import log
