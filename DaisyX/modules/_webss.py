a01cde6c25d6789840dbfd4a823c4c2c798c110a
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-05-08 11:51:20 +0530

    webss fixed

--- DaisyX/modules/_webss.py
+++ DaisyX/modules/_webss.py
@@ -16,15 +16,11 @@ async def take_ss(_, message):
         await message.reply_text("Give A Url To Fetch Screenshot.")
         return
     url = message.text.split(None, 1)[1]
-    m = await message.reply_text("**Taking Screenshot**")
-    await m.edit("**Uploading**")
+    m = await message.reply_text("**Taking Screenshot...**")
+    await m.edit("**Uploading Screenshot...**")
     try:
-        lel = await app.send_photo(
-            message.chat.id,
-            photo=f"https://webshot.amanoteam.com/print?q={url}",
-        )
+        await message.reply_photo(photo=f"https://webshot.amanoteam.com/print?q={url}", caption=f"Screenshot of {url}")
     except TypeError:
         await m.edit("No Such Website.")
         return
     await m.delete()
-    os.remove(lel)
