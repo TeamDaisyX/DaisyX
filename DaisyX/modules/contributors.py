b76ed0e122dc11075c133c17b2c6ab312001d992
InukaAsith <inukaasith7@gmail.com>
2021-06-01 01:50:35 +0000

    Auto Fixes: Code formatted

--- DaisyX/modules/contributors.py
+++ DaisyX/modules/contributors.py
@@ -15,10 +15,11 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import github #pyGithub
+import github  # pyGithub
 from pyrogram import filters
 
 from DaisyX.services.pyrogram import pbot as client
+
 
 @client.on_message(filters.command("contributors") & ~filters.edited)
 async def give_cobtribs(c, m):
