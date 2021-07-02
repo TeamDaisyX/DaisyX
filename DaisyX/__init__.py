94ff804235a6d4b8a3b3572bbfc512522fc2203d
Inuka Asith <inukaasith7@gmail.com>
2021-07-02 21:36:01 +0530

    Update __init__.py

--- DaisyX/__init__.py
+++ DaisyX/__init__.py
@@ -75,7 +75,7 @@ POSTGRESS_URL = get_str_key("DATABASE_URL", required=True)
 TEMP_DOWNLOAD_DIRECTORY = "./"
 
 # Sudo Users
-# SUDO_USERS = get_str_key("SUDO_USERS", required=True)
+SUDO_USERS = get_str_key("SUDO_USERS", required=True)
 
 # String Session
 STRING_SESSION = get_str_key("STRING_SESSION", required=True)
