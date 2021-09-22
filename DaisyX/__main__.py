129c757e6f414083d35d8300da4177a9af2aa2c4
Deshadeeth Thisarana <77653060+Deshadeeth-Thisarana@users.noreply.github.com>
2021-09-22 10:39:50 +0530

    Update __main__.py

--- DaisyX/__main__.py
+++ DaisyX/__main__.py
@@ -13,8 +13,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import asyncio
-import os
+import asyncio, os
 from importlib import import_module
 
 from aiogram import executor
