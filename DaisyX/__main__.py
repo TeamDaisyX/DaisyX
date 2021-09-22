972b376a4c255ced2d5cfee995156026f936402f
InukaAsith <inukaasith7@gmail.com>
2021-09-22 05:08:32 +0000

    Auto Fixes: Code formatted

--- DaisyX/__main__.py
+++ DaisyX/__main__.py
@@ -13,7 +13,8 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import os, asyncio
+import asyncio
+import os
 from importlib import import_module
 
 from aiogram import executor
